e0dcee53465b3c08474206669d2c795991a9f871
Kyle McAlpine <k@kylejm.io>
2017-10-29 11:29:56 +0000

    Improving report meta

--- Bugger/Device.swift
+++ Bugger/Device.swift
@@ -57,7 +57,52 @@ struct Device {
     }
     
     static var deviceModel: String {
-        return UIDevice.current.model
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        switch identifier {
+        case "iPod5,1":                                     return "iPod Touch 5"
+        case "iPod7,1":                                     return "iPod Touch 6"
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3":         return "iPhone 4"
+        case "iPhone4,1":                                   return "iPhone 4s"
+        case "iPhone5,1", "iPhone5,2":                      return "iPhone 5"
+        case "iPhone5,3", "iPhone5,4":                      return "iPhone 5c"
+        case "iPhone6,1", "iPhone6,2":                      return "iPhone 5s"
+        case "iPhone7,2":                                   return "iPhone 6"
+        case "iPhone7,1":                                   return "iPhone 6 Plus"
+        case "iPhone8,1":                                   return "iPhone 6s"
+        case "iPhone8,2":                                   return "iPhone 6s Plus"
+        case "iPhone9,1", "iPhone9,3":                      return "iPhone 7"
+        case "iPhone9,2", "iPhone9,4":                      return "iPhone 7 Plus"
+        case "iPhone8,4":                                   return "iPhone SE"
+        case "iPhone10,1", "iPhone10,4":                    return "iPhone 8"
+        case "iPhone10,2", "iPhone10,5":                    return "iPhone 8 Plus"
+        case "iPhone10,3", "iPhone10,6":                    return "iPhone X"
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":    return "iPad 2"
+        case "iPad3,1", "iPad3,2", "iPad3,3":               return "iPad 3"
+        case "iPad3,4", "iPad3,5", "iPad3,6":               return "iPad 4"
+        case "iPad4,1", "iPad4,2", "iPad4,3":               return "iPad Air"
+        case "iPad5,3", "iPad5,4":                          return "iPad Air 2"
+        case "iPad6,11", "iPad6,12":                        return "iPad 5"
+        case "iPad2,5", "iPad2,6", "iPad2,7":               return "iPad Mini"
+        case "iPad4,4", "iPad4,5", "iPad4,6":               return "iPad Mini 2"
+        case "iPad4,7", "iPad4,8", "iPad4,9":               return "iPad Mini 3"
+        case "iPad5,1", "iPad5,2":                          return "iPad Mini 4"
+        case "iPad6,3", "iPad6,4":                          return "iPad Pro 9.7 Inch"
+        case "iPad6,7", "iPad6,8":                          return "iPad Pro 12.9 Inch"
+        case "iPad7,1", "iPad7,2":                          return "iPad Pro 12.9 Inch 2. Generation"
+        case "iPad7,3", "iPad7,4":                          return "iPad Pro 10.5 Inch"
+        case "AppleTV5,3":                                  return "Apple TV"
+        case "AppleTV6,2":                                  return "Apple TV 4K"
+        case "AudioAccessory1,1":                           return "HomePod"
+        case "i386", "x86_64":                              return "Simulator"
+        default:                                            return identifier
+        }
     }
     
     static var orientation: String {
@@ -81,16 +126,26 @@ struct Device {
     }
     
     static var batteryLevel: String {
-        return "\(UIDevice.current.batteryLevel)%"
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        let level = "\(Int(device.batteryLevel * 100))%"
+        device.isBatteryMonitoringEnabled = false
+        return level
     }
     
     static var batteryState: String {
-        switch UIDevice.current.batteryState {
-        case .charging:     return "Charging"
-        case .full:         return "Full"
-        case .unknown:      return "Unknown"
-        case .unplugged:    return "Unplugged"
-        }
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        let state: String = {
+            switch device.batteryState {
+            case .charging:     return "Charging"
+            case .full:         return "Full"
+            case .unknown:      return "Unknown"
+            case .unplugged:    return "Unplugged"
+            }
+        }()
+        device.isBatteryMonitoringEnabled = false
+        return state
     }
     
     static var screenSize: String {
@@ -134,8 +189,7 @@ struct Device {
     }
     
     static var memoryCapacity: String {
-        // .phsicalMemory is in bytes, convert to Mb
-        return "\(ProcessInfo().physicalMemory / 1048576) Mb"
+        return "\(Int(ProcessInfo().physicalMemory).bytesToMegaBytes()) MB"
     }
     
     static var memoryUsed: String {
@@ -146,7 +200,7 @@ struct Device {
                 task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
             }
         }
-        return kerr == KERN_SUCCESS ? "\(info.resident_size) Mb" : ""
+        return kerr == KERN_SUCCESS ? "\(Int(info.resident_size).bytesToMegaBytes()) MB" : ""
     }
     
     static var fileSystemAttributes: [FileAttributeKey : Any]? {
@@ -159,11 +213,23 @@ struct Device {
     
     static var diskCapacity: String {
         guard let cap = fileSystemAttributes?[.systemSize] as? Int else { return  "" }
-        return String(describing: cap / 1048576)
+        return "\(cap.bytesToGigaBytes()) GB"
     }
     
     static var diskUsed: String {
         guard let used = fileSystemAttributes?[.systemFreeSize] as? Int else { return  "" }
-        return String(describing: used / 1048576)
+        return "\(used.bytesToGigaBytes()) GB"
+    }
+}
+
+private extension Int {
+    func bytesToMegaBytes() -> Float {
+        let bytesInMegaBytes: Float  = 1048576
+        return Float(self) / bytesInMegaBytes
+    }
+    
+    func bytesToGigaBytes() -> Float {
+        let bytesInGigaBytes: Float  = 1073741824
+        return Float(self) / bytesInGigaBytes
     }
 }
