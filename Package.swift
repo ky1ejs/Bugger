277c761d193d33d91e951e3515c3568b536f55cc
Kyle Satti <k@kylejs.dev>
2023-09-02 21:59:34 -0400

    Fix BuggerImgurStore dep

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "Bugger",dependencies: []),
         .target(name: "BuggerGitHub", dependencies: ["Bugger", "HelpfulUI"]),
-        .target(name: "BuggerImgurStore", dependencies: []),
+        .target(name: "BuggerImgurStore", dependencies: ["Bugger"]),
         .target(
             name: "BuggerLinear",
             dependencies: [
