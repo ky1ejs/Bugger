120d2d6a28956e5dcf50da683b9c80321fc6f004
Kyle Satti <k@kylejs.dev>
2023-09-02 22:12:31 -0400

    Fix missing BuggerImgurStore dep on BuggerGitHub

--- Package.swift
+++ Package.swift
@@ -23,7 +23,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "Bugger",dependencies: []),
-        .target(name: "BuggerGitHub", dependencies: ["Bugger", "HelpfulUI"]),
+        .target(
+            name: "BuggerGitHub",
+            dependencies: [
+                "Bugger",
+                "BuggerImgurStore",
+                "HelpfulUI"
+            ]),
         .target(name: "BuggerImgurStore", dependencies: ["Bugger"]),
         .target(
             name: "BuggerLinear",
