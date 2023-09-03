9a38cbddbde7b4687165c46a536b32f640548efb
Kyle Satti <k@kylejs.dev>
2023-09-03 17:30:18 -0400

    Remove graphql generated files (#10)
    * Remove graphql generated files
    
    * Add code gen step for linear target
    
    * Add package resolve step

--- Sources/BuggerLinear/LinearGraphQL/Schema/Interfaces/NotificationSubscription.graphql.swift
+++
@@ -1,9 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-extension LinearGraphQL.Interfaces {
-  /// Notification subscriptions for models.
-  static let NotificationSubscription = Interface(name: "NotificationSubscription")
-}
